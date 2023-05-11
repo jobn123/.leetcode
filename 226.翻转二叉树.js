705083350dd5c438f1f3e05d819e2170d5f35952
jobn123 <jobn_joy@163.com>
2023-05-11 10:37:18 +0800

    feat: 翻转二叉树

--- 226.翻转二叉树.js
+++ 226.翻转二叉树.js
@@ -22,15 +22,18 @@ var invertTree = function(root) {
     return null
   }
 
+  // solution 1
   // 交换左右子节点
-  const temp = root.left
-  root.left = root.right
-  root.right = temp
+  // const temp = root.left
+  // root.left = root.right
+  // root.right = temp
 
   // 左右子节点继续反转子节点
-  invertTree(root.left)
-  invertTree(root.right)
+  // invertTree(root.left)
+  // invertTree(root.right)
 
+  // solution 2
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)]
   return root
 
 };
