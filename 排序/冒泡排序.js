1815a4d0e34edf2f6d0fbc281947ededf85701d1
jobn123 <jobn_joy@163.com>
2023-05-15 10:44:25 +0800

    feat: 冒泡排序

--- 排序/冒泡排序.js
+++ 排序/冒泡排序.js
@@ -1,8 +1,12 @@
 const bubbleSort = (array) => {
+  // 遍历n次
   for (let i = 0; i < array.length; i++) {
+    // 每次遍历取最大值放到最后
+    // 可以优化的点是 array.length - 1 - i;
     for (let j = 0; j < array.length - 1 - i; j++) {
       const tmp = array[j];
-    
+      
+      // 交环位置
       if (tmp > array[j + 1]) {
         array[j] = array[j + 1];
         array[j + 1] = tmp;
